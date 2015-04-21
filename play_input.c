72037adac581fb515a38b6b2aa4620dab82c4b85
rkorakot <rkorakot@gmail.com>
2015-04-21 16:43:09 +0700

    commit day5/2

--- play_input.c
+++ play_input.c
@@ -52,7 +52,7 @@ int main()
     printf("5. Enter String(max 10 char) : ");
     scanf("%10s", str1);
     printf("   Your enter %s \n\n", str1);
-    
+    getchar();
 
     /* CASE 6 : Input โดยกำหนด format string
      | ถ้าต้องการรับข้อมูลทีละหลายๆตัวแปรในครั้งเดียวก็สามารถทำได้
@@ -63,6 +63,7 @@ int main()
     printf("6. Enter Your Full Name : ");
     scanf("%20s %20s", fname, sname);
     printf("   Your name is %s %s \n\n", fname, sname);
+    getchar();
 
 
     /* CASE 7 : Input แบบกำหนดให้ไม่สนใจ character ตาม format string
@@ -74,6 +75,7 @@ int main()
     //scanf("%d,%d,%d", &num1, &num2, &num3);
     scanf("%d%*c%d%*c%d", &num1, &num2, &num3);
     printf("   Your 3 Numbers: %d %d %d \n\n", num1, num2, num3);
+    getchar();
 
 
     /* CASE 8 : Scanset
@@ -86,17 +88,21 @@ int main()
     //scanf("%[ABCD]", str2); // รับเฉพาะตัวษร A B C D เท่านั้น (เฉพาะตัวเพิมพ์ใหญ่)
     //scanf("%[A-Z]", str2);  // รับเฉพาะตัวอักษร A ถึง Z เท่านั้น (เฉพาะตัวพิมพใหญ่)
     //scanf("%[a-z]", str2);  // รับเฉพาะตัวอักษร a ถึง z เท่านั้น (เฉพาะตัวพิมพ์เล็ก)
-    scanf("%[0-9]", str2);    // รับเฉพาะตัวเลข 0 ถึง 9 เท่านั้น) 
+    //scanf("%[A-Za-z ]", str2);  // รับเฉพาะตัวอักษร และ space
+    scanf("%[^\n]", str2);  // รับทุกตัวอักษรจนกระทังกด Enter
+    //scanf("%[0-9]", str2);    // รับเฉพาะตัวเลข 0 ถึง 9 เท่านั้น) 
     printf("Some character has been moved : %s \n\n", str2);
+    getchar();
 
     
-    /* CASE 8 : getchar();
+    /* CASE 9 : getchar();
      | getchar() จะรับข้อมูลแค่ character ตัวเดียวเท่านั้นไม่ว่าจะพิมพ์มาก๊่ตัวก็ตาม
     */
     char ch;
     printf("9. Enter Some Key : ");
     ch = getchar();
     printf(" Your typed key %c \n\n", ch);
+    //getchar();
     
     return 0;
 }
